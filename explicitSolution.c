c7df63a4bcba7f20c5c6ef2a0d574f60c90111ed
mblai35 <mbstrong35@gmail.com>
2016-10-07 22:07:32 -0600

    set-up
    Setting up a basic C script for the 1D explicit heat equation.

---
+++ explicitSolution.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  HeatEquation
+//
+//  Created by Mallory Lai on 10/7/16.
+//  Copyright © 2016 Mallory Lai. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+    
+    // Physical parameters
+    int L = 1.5; //length of muffin radius
+    
+    
+    // Numerical parameters
+    int dt; //change in time
+    int dx; //change in x
+    
+    
+    
+    //printf("Hello, World!\n");
+    
+    //free
+    
+    return 0;
+}
