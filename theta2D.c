7ef88c03743b4627c10028df61f0727126f20b1c
xhu4 <xhu4@uwyo.edu>
2016-10-30 20:26:00 -0400

    working on theta2D.c

--- theta2D.c
+++ theta2D.c
@@ -46,18 +46,21 @@
 #if defined(MALLORY)
     static const double par_cnt[4] = {90.49, -.06361, 80.13, -.001023};	/* parameters for boundary model */
     static const double par_bnd[4] = {73.97, -.08249, 81.56, -.001303};	/* parameters for central  model */
+//  static const double t0 =
 #elif defined(GEETA)
     static const double par_cnt[4] = {81.09, -.09036, 92.93, -.002168};	/* parameters for boundary model */
     static const double par_bnd[4] = {80.35, -.1156, 93.69, -.002442};	/* parameters for central  model */
+//  static const double t0 =
 #else
     static const double par_cnt[4] = {85.6,  -.09233, 90.35, -.002332}; /* parameters for boundary model */
     static const double par_bnd[4] = {63.76,  -.2109, 96.21, -.003575}; /* parameters for central  model */
+    static const double t0 = -1.97;
 #endif
 
 
+double** d2malloc( const int nx, const int ny, const char* s );
 double BoundaryModel( const double t );
 double CenterModel  ( const double t );
-double InitialModel ( const double x );
 void   CalcPar	    ( const int nx, const double **u, const double mu, const double THETA, double **e, double **f );
 void   CalcU1	    ( const int nx, const double **e, const double **f, double *u1 );
 
@@ -75,7 +78,7 @@ int main( int argc, char *argv[] ) {
     double  **e, **f;		/* parameters for Thomas Algorithm */
 
     FILE    *fp;                /* file pointer for output */
-    int	    i;                  /* loop index */
+    int	    i,j;                  /* loop index */
     int	    it;                 /* loop index */
     int	    ix,iy;              /* loop index */
 
@@ -114,8 +117,7 @@ int main( int argc, char *argv[] ) {
 
 
     /* Initialize parameters */
-    const double bnd0 = BoundaryModel(0);
-    const double cnt0 = CenterModel(0);
+    const double bnd0 = BoundaryModel(t0);
 
     const double ax = mux * THETA, ay = muy * THETA;
     const double bx = 2 * ax + 1,  by = 2 * ay + 1; 
@@ -124,48 +126,36 @@ int main( int argc, char *argv[] ) {
     /* Initialize x, t and u array */
     const int nx = L/DX + 1;
     const int ny = H/DY + 1;
-    const int nt = T/DT + 1;
+    const int nt = (T-t0)/DT + 1;
 
-    if ( (u = (double **) malloc( ny * sizeof(double *) )) == NULL ) {
-	perror("memory allocation for u");
-	free( u );
-	return -2;
-    }
-
-    for ( i = 0 ; i < ny ; i++ ) {
-	if ( (u[i] = (double *) malloc( nx * sizeof(double) )) == NULL ) {
-	    fprintf("memory allocation for u[%d]", i);
-	    for ( j = i-1 ; j>=0 ; j-- ) 
-		free( u[j] );
-	    free( u );
-	    return -2;
-	}
-    }
-    if ( (u1= (double **) malloc( ny * sizeof(double*) )) == NULL ) {
-	perror("memory allocation for u1");
-	return -2;
-    }
     if ( (t = (double *) malloc( nt * sizeof(double) )) == NULL ) {
-	perror("memory allocation for t");
-	return -2;
-    }
-    if ( (e = (double *) malloc( nx * sizeof(double) )) == NULL ) {
-	perror("memory allocation for u");
-	return -2;
-    }
-    if ( (f = (double *) malloc( nx * sizeof(double) )) == NULL ) {
-	perror("memory allocation for u");
 	return -2;
     }
 
-    for ( i = 0 ; i < nx ; i++ ) {
-	x[i] = i * DX;
-	u[i] = InitialModel( x[i] );
+    if ( (u = d2malloc(nx,ny,"u")) == NULL ) {
+	return -2;
     }
 
-    for ( i = 0 ; i < nt ; i++ ) {
-	t[i] = i * DT;
+    if ( (u1 = d2malloc(nx,ny,"u1")) == NULL ) {
+	return -2;
     }
+
+    if ( (e = d2malloc(nx,ny,"e")) == NULL ) {
+	return -2;
+    }
+
+    if ( (f = d2malloc(nx,ny,"f")) == NULL ) {
+	return -2;
+    }
+
+    t[0] = t0;
+    for ( i = 1 ; i < nt ; i++ ) {
+	t[i] = t[0] + DT;
+    }
+
+    for ( ix = 0 ; ix < nx ; ix++ ) 
+	for ( iy = 0 ; iy < ny ; iy++ ) 
+	    u[ix][iy] = bnd0;
 
 
     /* Open file for output */
@@ -178,10 +168,13 @@ int main( int argc, char *argv[] ) {
     /* Print initial condition to Theta1D.txt */
     fprintf(fp, "Time	   ");
     for ( ix = 0 ; ix < nx ; ix++ ) 
-	fprintf(fp, "| x = %4.2f ", x[ix]);
+	fprintf(fp, "| x = %4.2f ", ix*DX);
     fprintf(fp, "\n%-6.3f(min)", t[0]);
-    for ( ix = 0 ; ix < nx ; ix++ ) 
-	fprintf(fp, "|%9.5f ", u[ix]);
+    for ( iy = 0 ; iy < ny ; iy++ ) { 
+	for ( ix = 0 ; ix < nx ; ix++ ) 
+	    fprintf(fp, "|%9.5f ", u[ix]);
+	fprintf(fp, "\n");
+    }
     fprintf(fp, "\n");
 
 
@@ -227,12 +220,7 @@ double CenterModel ( const double t ) {
 }
 
 
-double InitialModel ( const double x ) {
-    return (bnd0 - cnt0) * 4.0 / (L*L) * (x - L/2.0) * (x - L/2.0) + cnt0;
-}
-
-
-void   CalcPar ( const int nx, const double *u, const double mu, const double THETA, double *e, double *f ) {
+void   CalcPar ( const int nx, const double **u, const double mu, const double THETA, double **e, double **f, const char mode ) {
     int i;
     double d;
     for ( i = 1 ; i < nx-1 ; i++ ) {
@@ -240,4 +228,26 @@ void   CalcPar ( const int nx, const double *u, const double mu, const double TH
 	e[i] = a / (b - a * e[i-1]);
 	f[i] = (d + a * f[i-1]) / (b - a * e[i-1]);
     }
+}
+
+
+double** d2malloc( const int nx, const int ny, const char* s ){
+    int	     i,j;
+    double** pb;
+    if ( (pb = (double**) malloc( ny * sizeof(double *) )) == NULL ) {
+	fprintf(stderr,"memory allocation for %s" ,s);
+	return NULL;
+    }
+
+    for ( i = 0 ; i < ny ; i++ ) {
+	if ( (pb[i] = (double*) malloc( nx * sizeof(double) )) == NULL ) {
+	    fprintf(stderr,"memory allocation for %s[%d]",s,i);
+	    for ( j = i-1 ; j >=0 ; j-- ) {
+		free(pb[j]);
+	    }
+	    free(pb);
+	    return NULL;
+	}
+    }
+    return pb;
 }
